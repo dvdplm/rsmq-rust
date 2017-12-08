822c0d1216045d2fd4f6ddffd32519bd4630d425
David Palm <dvdplm@gmail.com>
2017-12-08 17:16:04 +0100

    Test most of the public API

--- tests/rsmq_test.rs
+++ tests/rsmq_test.rs
@@ -44,13 +44,92 @@ fn delete_queue() {
 #[test]
 fn send_message() {
   let rsmq = setup("test-ns");
-  let queue_stats_before = rsmq.get_queue_attributes("test-q").expect("fetch queue stats BEFORE failed");
-  let message_id = rsmq.send_message("test-q", "fancy schmancy message", None);
-  let queue_stats_after = rsmq.get_queue_attributes("test-q").expect("fetch queue stats AFTER failed");
+  let qname = "test-send-message-q";
+  rsmq.delete_queue(qname).expect("no queue deleted");
+  rsmq.create_queue(rsmq::Queue::new(qname, Some(0), Some(0), None)).expect("can't create queue");
+  let queue_stats_before = rsmq.get_queue_attributes(qname).expect("fetch queue stats BEFORE failed");
+  let message_id = rsmq.send_message(qname, "fancy schmancy message", None);
+  let queue_stats_after = rsmq.get_queue_attributes(qname).expect("fetch queue stats AFTER failed");
 
   assert!(message_id.is_ok());
   assert_eq!(
     queue_stats_before.msgs+1,
     queue_stats_after.msgs
   )
+}
+
+#[test]
+fn delete_message() {
+  let rsmq = setup("test-ns");
+  let qname = "test-delete-msg-q";
+  rsmq.delete_queue(qname).expect("no queue deleted");
+  rsmq.create_queue(rsmq::Queue::new(qname, None, None, None)).expect("can't create queue");
+  
+  let queue_stats_before = rsmq.get_queue_attributes(qname).expect("fetch queue stats BEFORE failed");
+
+  let message_id = rsmq.send_message(qname, "fancy schmancy message", None);
+  assert!(message_id.is_ok());
+
+  let queue_stats_after = rsmq.get_queue_attributes(qname).expect("fetch queue stats AFTER failed");
+
+  let deleted = rsmq.delete_message(qname, &message_id.unwrap());
+  assert!(deleted.is_ok() && deleted.unwrap());
+  let queue_stats_after_delete = rsmq.get_queue_attributes(qname).expect("fetch queue stats AFTER failed");
+  
+  assert_eq!(
+    queue_stats_before.msgs+1,
+    queue_stats_after.msgs
+  );
+
+  assert_eq!(
+    queue_stats_after.msgs -1,
+    queue_stats_after_delete.msgs
+  );
+}
+
+#[test]
+fn pop_message() {
+  let rsmq = setup("test-ns");
+  let qname = "pop-message-q";
+  rsmq.delete_queue(qname).expect("no queue deleted");
+  rsmq.create_queue(Queue::new(qname, None, None, None)).expect("no queue for you!");
+
+  let mid = rsmq.send_message(qname, "poppy message", None);
+  assert!(mid.is_ok());
+
+  let queue_stats_before = rsmq.get_queue_attributes(qname).expect("fetch queue stats BEFORE failed");
+  let popped = rsmq.pop_message(qname);
+  let queue_stats_after = rsmq.get_queue_attributes(qname).expect("fetch queue stats AFTER failed");
+
+  assert!(popped.is_ok());
+  assert_eq!(popped.unwrap().id, mid.unwrap());
+  assert_eq!(queue_stats_after.msgs, 0);
+  assert_eq!(queue_stats_before.msgs, 1);
+  assert_eq!(queue_stats_after.hiddenmsgs, 0);
+}
+
+#[test]
+fn receive_message() {
+  let rsmq = setup("test-ns");
+  let qname = "receive-message-q";
+  rsmq.delete_queue(qname).expect("no queue deleted");
+  rsmq.create_queue(Queue::new(qname, None, None, None)).expect("no queue for you!");
+  let mid = rsmq.send_message(qname, "reccy message", Some(0));
+  assert!(mid.is_ok());
+  std::thread::sleep(std::time::Duration::from_millis(800)); // wait for messages to become unhidden
+
+  let queue_stats_before = rsmq.get_queue_attributes(qname).expect("fetch queue stats BEFORE failed");
+  let reserved = rsmq.receive_message(qname, None);
+  assert!(reserved.is_ok());
+  assert_eq!(reserved.unwrap().id, mid.unwrap());
+  let queue_stats_after = rsmq.get_queue_attributes(qname).expect("fetch queue stats AFTER failed");
+
+  assert_eq!(queue_stats_before.msgs, 1);
+  assert_eq!(queue_stats_after.msgs, 1); // reserving a message does not delete it
+
+  assert_eq!(queue_stats_after.totalrecv, 1);
+  assert_eq!(queue_stats_before.totalrecv, 0);
+
+  assert_eq!(queue_stats_after.hiddenmsgs, 1); // reserving a message hides it from others for queue.vt seconds
+  assert_eq!(queue_stats_before.hiddenmsgs, 0);
 }
